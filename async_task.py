4b5721f1a64506580312211697b373b81f054bf0
kiritepungaupp <2024116ung@aupp.edu.kh>
2024-09-25 12:05:47 +0700

    finished

--- async_task.py
+++ async_task.py
@@ -1,7 +1,19 @@
 import asyncio
 
-async def async_write_to_file(filename, data, duration):
-    pass
+async def async_write_to_file(filename, data):
+    with open(filename, 'w') as file:
+        for line in data:
+            file.write(str(line)+'\n')
 
-async def run_async_tasks():
-    pass
+async def run_async_tasks(primes):
+    filename = [1,2,3,4,5,6,7,8,9,10]
+
+    dataClean = [primes[i::10] for i in range(10)]
+
+    
+    tasks = [async_write_to_file(str(str(filename[i])+'.txt'), dataClean[i]) for i in range(10) ]
+
+    # # Await the completion of all write tasks
+    await asyncio.gather(*tasks)
+
+
--- multiprocessing_task.py
+++ multiprocessing_task.py
@@ -1,10 +1,30 @@
 import multiprocessing
 
+
+
+primesofall = []
+
 def is_prime(n):
-  pass
 
-def check_prime_chunk(numbers):
-  pass
 
-def find_primes_in_range(numbers, chunk_size):
-  pass
+    cpus = multiprocessing.cpu_count()
+
+    with multiprocessing.Pool(processes=cpus) as pool:
+        results = pool.starmap(check_prime, [(x,5,int(x**0.5) + 1) for x in n])
+    return results
+
+def check_prime(number,start,end):
+    global primesofall
+    if number <= 1 or (number % 2 == 0 and number > 2) or (number % 3 == 0 and number > 3):
+        return 0
+    for i in range(start, end, 6):
+        if number % i == 0 or number % (i + 2) == 0:
+            return 0
+    return number
+
+def find_primes_in_range(numbers):
+    results = is_prime(numbers)
+    
+    return([x for x in results if x!= 0])
+    
+
